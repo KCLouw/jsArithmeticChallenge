62e44002ebf9b1561cf07b1d3f2857614710283b
KCLouw <klouw07@gmail.com>
2023-01-19 21:54:58 +0200

    Created html and js file for the challenge
    started and finished the JS Arithmetic challenge.

---
+++ scripts/js-arithmetic.js
@@ -0,0 +1,46 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+// rectangle measurements
+const length = 10;
+const breadth = 17;
+
+// circle measurement
+const radius = 25;
+
+// triangle measurements
+const base = 20;
+const height = 9;
+
+
+// Rectangle area calculation
+   const rectangleArea = length * breadth;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " + rectangleArea );
+
+
+console.log("\n");
+
+
+// circle area calculation
+   const circleArea = pi * radius ** 2;
+
+// Append your circle answer:
+console.log("The area of the given circle is : " + circleArea);
+
+
+console.log("\n");
+
+
+// triangle area calculation
+   const triangleArea = (half * base) * height;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " + triangleArea );
